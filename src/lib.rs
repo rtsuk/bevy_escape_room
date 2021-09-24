05912203663a95151d59e91f019f6fa52eab2bb2
Rob Tsuk <rob@tsuk.com>
2021-09-24 06:56:33 -0700

    Inventory and mouse move

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 use bevy::prelude::*;
-use wasm_bindgen::prelude::*;
+use bevy_egui::{egui, EguiContext, EguiPlugin};
 use bevy_flycam::PlayerPlugin;
+use wasm_bindgen::prelude::*;
 
+const INVENTORY_TEXTURE_ID: u64 = 0;
 
 pub fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn_scene(asset_server.load("er.gltf#Scene0"));
@@ -28,14 +30,37 @@ pub fn rotator_system(time: Res<Time>, mut query: Query<&mut Transform, With<Rot
     }
 }
 
+fn load_assets(mut egui_context: ResMut<EguiContext>, assets: Res<AssetServer>) {
+    let texture_handle = assets.load("inventory_slot.png");
+    egui_context.set_egui_texture(INVENTORY_TEXTURE_ID, texture_handle);
+}
+
+fn ui_example(egui_context: Res<EguiContext>) {
+    egui::Window::new("Inventory")
+        .default_width(100.0)
+        .show(egui_context.ctx(), |ui| {
+            ui.vertical_centered(|ui| {
+                for _ in 0..5 {
+                    ui.add(egui::widgets::Image::new(
+                        egui::TextureId::User(INVENTORY_TEXTURE_ID),
+                        [80.0, 80.0],
+                    ));
+                }
+            });
+        });
+}
+
 #[wasm_bindgen]
 pub fn run() {
     let mut app = App::build();
 
     app.add_plugins(DefaultPlugins);
     app.add_plugin(PlayerPlugin);
+    app.add_plugin(EguiPlugin);
+    app.add_startup_system(load_assets.system());
     app.add_startup_system(crate::setup.system());
     app.add_system(rotator_system.system());
+    app.add_system(ui_example.system());
 
     // when building for Web, use WebGL2 rendering
     #[cfg(target_arch = "wasm32")]
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
 fn main() {
     bevy_er::run()
 }
-
